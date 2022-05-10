cde9b9d47b4f3ae6ebf3acab19e75a686e73fdde
askepen <aske@vhg26.dk>
2022-05-10 12:16:52 +0200

    Test script updated to match API

--- python/test.py
+++ python/test.py
@@ -2,6 +2,7 @@ from regicide import actions, card, players
 import regicide
 import random
 
+
 class CustomPlayer:
     def __init__(self) -> None:
         pass
@@ -19,29 +20,34 @@ class CustomPlayer:
 
         print(f"{state.reward()}: {state.current_enemy()}")
         print([x.__str__() for x in state.current_hand()])
-            
+
         print(action)
         print()
-        
+
         return action
-            
+
+
 class CustomMCTSPlayer:
     def __init__(self) -> None:
-        self.base = regicide.players.MCTSPlayer(n_playouts=1000, use_heuristics=True)
+        self.base = regicide.players.MCTSPlayer(
+            playouts=1000, use_heuristics=True, num_threads=4
+        )
 
     def play(self, state):
         action = self.base.play(state)
         print(f"{state.reward()} -> {action}")
         return action
-    
+
+
 players = [
     CustomMCTSPlayer(),
+    CustomMCTSPlayer(),
+    CustomMCTSPlayer(),
     # CustomPlayer(),
-    # players.MCTSPlayer(n_playouts=10_000, use_heuristics=False),
+    # players.MCTSPlayer(n_playouts=1_000, use_heuristics=False),
     # players.InputPlayer(),
 ]
 
-game = regicide.RegicideGame(players, 1337)
+game = regicide.RegicideGame(players)
 result = game.playout()
 print(f"{result = }, reward = {game.reward()}")
-
